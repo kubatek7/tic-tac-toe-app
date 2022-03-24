34f4f52e57741d01979f452764152deb2cf30618
kubatek7 <jakub@purplebeard.co.uk>
2022-03-24 12:34:27 +0000

    corrected error on src/component/board.js

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,22 +1,12 @@
-import React from "react";
-import Square from "./Square";
+import React from 'react';
+import Square from "./Square"; 
 
-export function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
+const Board = ({ squares, onClick }) => (
+  <div className="board">
+    {squares.map((square, i) => (
+      <Square key={i} value={square} onClick={() => onClick(i)} />
+    ))}
+  </div>
+);
+
+export default Board;
