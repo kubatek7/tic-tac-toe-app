d0f60b126027decdeb715652e449058f427d5fc1
kubatek7 <jakub@purplebeard.co.uk>
2022-03-25 21:53:18 +0000

    new update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-import React from "react";
-import ReactDom from "react-dom";
-import "./index.css";
-import Game from "./components/Game";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-ReactDom.render(<Game />, document.getElementById("root"));
+//import { BrowserRouter, Routes, Route } from 'react';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
---
+++ src/scoreBoard/score_Board.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function score_Board() {
+  return (
+    <div>score_Board</div>
+  )
+}
+
+export default score_Board;
